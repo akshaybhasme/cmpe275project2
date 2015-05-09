475794d1cab165680ce8f1dbf3dd5d6bc1d51563
Apurva Pawar <theapurvapawar@gmail.com>
2015-05-09 13:51:43 -0700

    Init files for packages, Item and Customer class update

--- Models/Item.py
+++ Models/Item.py
@@ -1,5 +1,19 @@
+from random import randint
+
+MAX_ITEM_VALUE = 50
+ITEM_ALCOHOL_PROBABILITY = 10
+
+
 class Item:
     def __init__(self):
         self.name = ""
         self.value = 0
         self.isAlcoholic = False
+        self.generate_item()
+
+    def generate_item(self):
+        self.name = "Item Number " + randint(1, 5000)
+        self.value = randint(1, MAX_ITEM_VALUE)
+
+        if randint(0, 100) < ITEM_ALCOHOL_PROBABILITY:
+            self.isAlcoholic = True
