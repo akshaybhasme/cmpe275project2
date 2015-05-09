f17a5600250e45bc64ac4de88860684102c13e52
sidwarang <siddharth.war@gmail.com>
2015-05-09 13:12:31 -0700

    Added state class to root

---
+++ Actors/Counter.py
@@ -0,0 +1,11 @@
+__author__ = 'Siddharth'
+
+import state
+class Counter(object):
+    def __init__(self):
+            print("init")
+
+
+class On(state):
+    def __init__(self):
+            print("init")
