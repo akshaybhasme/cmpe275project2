0353740a34bd8d0643f4d4d19d04d4b91907df9d
sidwarang <siddharth.warang@sjsu.edu>
2015-05-13 16:24:59 -0700

    modified conversations

--- client/AttendantClient.py
+++ client/AttendantClient.py
@@ -25,7 +25,7 @@ class AttendantClientProtocol(LineReceiver):
     def dataReceived(self, data):
         total = 0
         alcoholic = False
-        print data
+        # print data
         try:
             message = json.loads(data)
             # print("Message received")
@@ -94,6 +94,7 @@ class AttendantClientProtocol(LineReceiver):
 
             msg_json = json.dumps(msg, default=lambda o: o.__dict__)
             self.sendLine(msg_json)
+            time.sleep(2)
 
         except Exception as e:
             print e.message
